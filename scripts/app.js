e07c621dd6db535cf135300ba07ae9df58593f17
Simon Meskens <meskens.simon@gmail.com>
2014-11-06 02:50:53 +0100

    fixed caching

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 /* global requirejs, require */
 
 require.config({
-    //urlArgs: Date.now(),
+    urlArgs: Date.now(),
     paths: {
         "keypress": "https://rawgit.com/dmauro/Keypress/2.0.3/keypress-2.0.3.min.js?",
         "Composer": "https://rawgit.com/jtenner/Composer.js/master/Composer.js?"
